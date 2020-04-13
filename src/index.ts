2e3ad20ba5fc2055b667e5e171319f881f3f3859
Andrew Powers <andrew@fiction.com>
2020-04-13 14:38:18 -0700

    fix: TS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export default {}
+import { addContentRoutes } from "@factor/api"
+
+const itemView = () => import("./views/v-item.vue")
+const userView = () => import("./views/v-user.vue")
+const listView = () => import("./views/list-item.vue")
+
+addContentRoutes({
+  key: "appRoutes",
+  routes: [
+    { path: "/i/:view/:page", component: listView },
+    { path: "/item/:id", component: itemView },
+    { path: "/user/:id", component: userView },
+    { path: "/", redirect: "/i/top" }
+  ]
+})
